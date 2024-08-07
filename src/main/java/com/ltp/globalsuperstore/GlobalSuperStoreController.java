cacf624e65a8201e72ee41c2ec3e3addb3f9983e
DeCommer <jdecommer@gmail.com>
2024-08-07 09:03:00 -0400

    Cannot get delete to work

--- src/main/java/com/ltp/globalsuperstore/GlobalSuperStoreController.java
+++ src/main/java/com/ltp/globalsuperstore/GlobalSuperStoreController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 // import com.ltp.globalsuperstore.Item;
 
@@ -25,14 +26,15 @@ public class GlobalSuperStoreController {
     }
 
     @PostMapping("/submitItem")
-    public String submitItemHandler(Item item) {
+    public String submitItemHandler(Item item, RedirectAttributes redirectAttributes) {
         int idx = getIdxFromId(item.getId());
-        
+
         if (idx == Constants.NOT_FOUND) {
             items.add(item);
         } else {
             items.set(idx, item);
         }
+        // redirectAttributes.addFlashAttribute("status", Constants.SUCCESS_STATUS);
         return "redirect:/inventory";
     }
 
@@ -48,6 +50,5 @@ public class GlobalSuperStoreController {
         }
         return Constants.NOT_FOUND;
     }
-
 
 }
